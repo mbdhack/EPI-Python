bb43b4d01de8e7a2371e158dcb10d5b6d465d7a1
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-07-31 13:08:07 -0500

    solve chapt 1

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -3,7 +3,11 @@ from test_framework import generic_test
 
 def parity(x):
     # TODO - you fill in here.
-    return 0
+    result = 0
+    while x:
+        result ^= 1 #1 or 0 toggle based on what 
+        x &= (x-1) #drop lowest set bit
+    return result
 
 
 if __name__ == '__main__':
