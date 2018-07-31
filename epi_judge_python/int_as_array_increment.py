224206dfe754baf320825e76953b762cc85b3026
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-07-31 16:12:44 -0500

    array

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def plus_one(A):
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1 # add one to the last element of A
+
+    for i in reversed(range(1,len(A))):
+        if A[i] != 10: # the last element was not 9(max)
+            break
+        # there is a carry
+        #set the digit to zero and add the carry to the digit before it
+        A[i] =0
+        A[i-1] += 1
+    # if the first digit is 10, a cary was applied
+    #set it to one and add 0.
+    if A[0] == 10:
+        A[0]=1
+        A.append(0)
+    return A
 
 
 if __name__ == '__main__':
