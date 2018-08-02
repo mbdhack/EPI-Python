e74ab50ac2bf632816147d26799437e8170b2b0d
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-08-02 10:53:53 -0500

    solving string

--- epi_judge_python/solved_buy_and_sell_stock.py
+++ epi_judge_python/solved_buy_and_sell_stock.py
@@ -4,9 +4,9 @@ from test_framework import generic_test
 def buy_and_sell_stock_once(prices):
     min_price_so_far,max_profit = float('inf'),0
     for price in prices:
-        max_profit_for_now = price-min_price_so_far
-        min_price_so_far = min(price,min_price_so_far)
-        max_profit = max(max_profit_for_now,max_profit)
+        max_profit_for_now = price-min_price_so_far #at the current price, what is my profit
+        min_price_so_far = min(price,min_price_so_far) #what is the minimum price I have come accross so far
+        max_profit = max(max_profit_for_now,max_profit) # what is the maximum overall 
     return max_profit
 
 
